d389ed3b8c4d985c441e62a30704cedcc495036a
Conor Sheehan <conor@cantinaconsulting.com>
2018-08-14 18:01:35 -0400

    Make navigation work

--- public/script.js
+++ public/script.js
@@ -1,48 +1,75 @@
-var hiddenClass = 'hidden';
-
 var app = (function() {
+  var hiddenClass = 'hidden';
+  var visibleClass = 'visible';
+  var navVisibleClass = 'nav-shown';
+
   return {
     initialize: function() {
-      this.$header = $('header');
-      this.$footer = $('footer > div');
-      this.$ampersand = $('.ampersand');
+      this.$menu = $('a.menu');
+      this.$nav = $('.full-nav');
+      this.$body = $('body');
+      this.addHandlers();
       return this;
     },
 
-    hideAll: function() {
-      this.$header.addClass(hiddenClass);
-      this.$footer.addClass(hiddenClass);
-      this.$ampersand.addClass(hiddenClass);
-      return this;
-    },
-
-    showAll: function() {
+    addHandlers: function() {
       var self = this;
-      setTimeout(function() { self.showHeader(); }, 500);
-      setTimeout(function() { self.showAmpersand(); }, 500);
-      setTimeout(function() { self.showFooter(); }, 2000);
+      this.$menu.on('click', function(e) {
+        e.preventDefault();
+        self.showNav();
+      });
+      $('.full-nav .close').on('click', function(e) {
+        e.preventDefault();
+        self.hideNav();
+      });
       return this;
     },
 
-    showHeader: function() {
-      this.$header.removeClass(hiddenClass);
+    showNav: function() {
+      this.$nav.addClass(visibleClass);
+      this.$body.addClass(navVisibleClass);
+      $(window).scrollTop(0);
       return this;
     },
 
-    showFooter: function() {
-      this.$footer.removeClass(hiddenClass);
+    hideNav: function() {
+      this.$nav.removeClass(visibleClass);
+      this.$body.removeClass(navVisibleClass);
       return this;
     },
 
-    showAmpersand: function() {
-      this.$ampersand.removeClass(hiddenClass);
-      return this;
-    }
+    // hideAll: function() {
+    //   this.$header.addClass(hiddenClass);
+    //   this.$footer.addClass(hiddenClass);
+    //   this.$ampersand.addClass(hiddenClass);
+    //   return this;
+    // },
+
+    // showAll: function() {
+    //   var self = this;
+    //   setTimeout(function() { self.showHeader(); }, 500);
+    //   setTimeout(function() { self.showAmpersand(); }, 500);
+    //   setTimeout(function() { self.showFooter(); }, 2000);
+    //   return this;
+    // },
+
+    // showHeader: function() {
+    //   this.$header.removeClass(hiddenClass);
+    //   return this;
+    // },
+
+    // showFooter: function() {
+    //   this.$footer.removeClass(hiddenClass);
+    //   return this;
+    // },
+
+    // showAmpersand: function() {
+    //   this.$ampersand.removeClass(hiddenClass);
+    //   return this;
+    // }
   };
 })();
 
 $(function() {
-  app.initialize()
-    .hideAll()
-    .showAll();
+  app.initialize();
 });
